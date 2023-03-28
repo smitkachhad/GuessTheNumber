c23556eb455caf0f017d1a22e764f88356839683
smitkachhad <129096485+smitkachhad@users.noreply.github.com>
2023-03-28 15:13:43 +0530

    Update Guess_The_Number.java

--- Guess_The_Number.java
+++ Guess_The_Number.java
@@ -47,15 +47,6 @@ class Game
 }
 public class Guess_The_Number {
     public static void main(String[] args) {
-        /*
-            Create a class Game, which allows a user to play "Guess the Number" game once.
-            Game should have the following methods:
-            Constructor to generate the random number
-            takeUserInput() to take a user input of number
-            isCorrectNumber() to detect whether the number entered by the user is true
-            getter and setter for noOfGuesses
-            Use properties such as noOfGuesses(int), etc to get this task done!
-         */
         Game g=new Game();
         boolean b=false;
         while (!b) {
